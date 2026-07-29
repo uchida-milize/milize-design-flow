84d8801da6591f57b541be2c645a41e3fc8487c2
uchida-milize <68524311+uchida-milize@users.noreply.github.com>
2026-07-29 13:40:08 +0900

    Update layout.tsx

--- src/app/sharp/layout.tsx
+++ src/app/sharp/layout.tsx
@@ -1,6 +1,4 @@
 import './globals.css';
-export default function Layout({
-  children,
-}: { children: React.ReactNode }) {
-  return <>{children}</>;
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <div className="sharp-portal">{children}</div>;
 }
