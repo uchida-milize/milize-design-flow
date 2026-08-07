2e28fee315227324841d0b24728b6972ba3481a7
uchida-milize <68524311+uchida-milize@users.noreply.github.com>
2026-08-07 19:18:28 +0900

    Update page.tsx

--- src/app/milize-asset-portal/page.tsx
+++ src/app/milize-asset-portal/page.tsx
@@ -2,7 +2,7 @@ import { ClientPortalHeader } from '@/components/ClientPortalHeader';
 
 const clientName = 'Milize Asset Portal';
 const basePath = '/milize-asset-portal';
-const primaryColor: string = '#C0C0C0';
+const primaryColor = 'var(--primary-color)';
 
 const colorRatio = [
   { hex: '#C0C0C0', name: 'シルバー', percent: 35 },
