3d8c84d758667c7a74ba5ef47e4ef96de155353c
uchida-milize <68524311+uchida-milize@users.noreply.github.com>
2026-09-08 18:04:44 +0900

    feat: delete client portal toyota

--- src/app/toyota/layout.tsx
+++
@@ -1,5 +0,0 @@
-import './globals.css';
-import type { ReactNode } from 'react';
-export default function Layout({ children }: { children: ReactNode }) {
-  return <div className="toyota-portal">{children}</div>;
-}
