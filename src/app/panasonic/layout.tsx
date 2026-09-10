25ebfa513831937d6e5f8e0f8c89438e8895745c
uchida-milize <68524311+uchida-milize@users.noreply.github.com>
2026-09-10 13:12:33 +0900

    feat: delete client portal panasonic

--- src/app/panasonic/layout.tsx
+++
@@ -1,5 +0,0 @@
-import './globals.css';
-import type { ReactNode } from 'react';
-export default function Layout({ children }: { children: ReactNode }) {
-  return <div className="panasonic-portal">{children}</div>;
-}
